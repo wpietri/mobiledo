3962d5da5caafc2cc0784a08d1f41096a352435c
William Pietri <william@scissor.com>
2014-06-13 12:31:31 -0700

    Fixing data initialization issue

--- www/js/app.js
+++ www/js/app.js
@@ -8,29 +8,23 @@ angular.module('todo', ['ionic'])
 
     .factory('Projects', function() {
         return {
-            all: function() {
-                var projectString = window.localStorage['projects'];
-                if(projectString) {
-                    return angular.fromJson(projectString);
-                }
-                return [newProject("tasks")];
-            },
-            save: function(projects) {
-                window.localStorage['projects'] = angular.toJson(projects);
-            },
             newProject: function(projectTitle) {
                 // Add a new project
                 return {
                     title: projectTitle,
                     tasks: []
                 };
-            }//,
-//            getLastActiveIndex: function() {
-//                return parseInt(window.localStorage['lastActiveProject']) || 0;
-//            },
-//            setLastActiveIndex: function(index) {
-//                window.localStorage['lastActiveProject'] = index;
-//            }
+            },
+            all: function() {
+                var projectString = window.localStorage['projects'];
+                if(projectString) {
+                    return angular.fromJson(projectString);
+                }
+                return [this.newProject("work"),this.newProject("home")];
+            },
+            save: function(projects) {
+                window.localStorage['projects'] = angular.toJson(projects);
+            }
         }
     })
 
